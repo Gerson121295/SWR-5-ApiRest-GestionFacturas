da2fb8ad7df51168700f72b6f250254fe2ec002c
Gerson121295 <gerson21millones@gmail.com>
2025-01-31 15:11:29 -0600

    01-Creacion de paquetesy dependencias del proyecto

---
+++ src/main/java/com/api/gestion/facturas/P5GestionFacturasApplication.java
@@ -0,0 +1,13 @@
+package com.api.gestion.facturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P5GestionFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P5GestionFacturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/gestion/facturas/P5GestionFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gestion.facturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P5GestionFacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
